1dc3552d65062e228c045adcb22b30624b23c437
satya <github846@gmail.com>
2021-08-27 20:26:06 +0530

    google home page clone

---
+++ google page clone/script.js
@@ -0,0 +1,27 @@
+const burger = document.querySelector(".rightmenu i");
+const appscon = document.querySelector(".googleapps-con");
+const user = document.querySelector(".rightmenu img");
+const usercon = document.querySelector(".account");
+appscon.style.display='none';
+usercon.style.display='none';
+burger.addEventListener("click", () => {
+  if (appscon.style.display !== "flex") {
+    appscon.style.display = "flex";
+    if (usercon.style.display !== "none") {
+      usercon.style.display = "none";
+    }
+  } else {
+    appscon.style.display = "none";
+  }
+});
+
+user.addEventListener("click", () => {
+  if (usercon.style.display !== "block") {
+    usercon.style.display = "block";
+    if (appscon.style.display !== "none") {
+      appscon.style.display = "none";
+    }
+  } else {
+    usercon.style.display = "none";
+  }
+});
